3ef93a81438a72d6524eaa139dd60dbb3b426347
maazmunir <maaz.munir@gmail.com>
2020-06-07 16:44:47 +0500

    first commit

---
+++ Chapter 1/app.js
@@ -0,0 +1,20 @@
+// Ch#1 Q#1
+
+alert("Welcome!");
+
+// Ch#1 Q#2
+
+alert("Error! Please enter a valid password.");
+
+// Ch#1 Q#3
+
+alert("Welcome to JS Land \nHappy Coding");
+
+// Ch#1 Q#4
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Ch#1 Q#5
+
+Console.Log("Hello... I can run JS through my web browser's console");
---
+++ Chapter 2/app.js
@@ -0,0 +1,39 @@
+// Ch#2 Q 1
+var username;
+
+// Ch#2 Q 2
+var myName = "M. Maaz M. Munir";
+
+// Ch#2 Q 3
+var message;
+message = "Hello World";
+alert(message);
+
+// Ch#2 Q 4
+var q1 = prompt("Enter Your Name?", "write here");
+var q2 = prompt("Enter Your Age?", "write here");
+var q3 = prompt("Enter Your Education?", "write here");
+alert("Your name is " + q1);
+alert("Your Age is " + q2);
+alert("Your Education is " + q3);
+
+//Ch#2 Q 5
+var x = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(x);
+
+//Ch#2 Q 6
+var email = "example@example.com";
+alert("My email address is " + email);
+
+// Ch#2 Q 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Ch#2 Q8
+document.write("Yah! I can write HTML content through JavaScript");
+
+// Ch#2 Q9
+var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(a);
+
+
